2712a240d5a128e22f52211c802bdc2ee653db91
GithubActions <donotemailme@example.com>
2021-04-22 00:00:56 +0000

    v0.0.5+58.g8d6e373 docs

---
+++ versions/latest/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v0.0.5+58.g8d6e373.dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
